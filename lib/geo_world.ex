91a3fd4c4737d02cb53abafde2769b9afb1b04b0
Getty Orawo <gettyorawo@gmail.com>
2021-05-25 15:45:23 +0300

    persist CSV content and return analysis

--- lib/geo_world.ex
+++ lib/geo_world.ex
@@ -2,6 +2,8 @@ defmodule GeoWorld do
   @moduledoc """
   A module for geolocation information
   """
+  alias GeoWorld.Geolocation
+  alias GeoWorld.Repo
 
   @doc """
   Takes input from CSV file and decodes each line into a map as a record
@@ -13,5 +15,26 @@ defmodule GeoWorld do
     |> CSV.decode(separator: ?,, headers: true)
     |> Enum.take(10) #only for the first 10 records
     |> Keyword.get_values(:ok)
+  end
+
+
+  def persist_record([], accepted, rejected, {current, total}) do
+    %{accepted: accepted, rejected: rejected, total_records: total}
+  end
+
+  def persist_record(data, accepted, rejected, {current, total}) do   
+    [params | rem] = data 
+    changeset = Geolocation.changeset(%Geolocation{}, params)
+    case Repo.insert(changeset) do
+      {:ok, geolocation} -> 
+        persist_record(rem, accepted+1, rejected, {current + 1, total})
+      {:error, changeset} -> 
+        persist_record(rem, accepted, rejected+1, {current + 1, total})
+    end
+  end
+
+  def import_csv do
+    data = decode_csv()
+    {time_in_micro, res} = :timer.tc(fn -> persist_record(data, 0 ,0 , {0, Enum.count(data)}) end)
   end
 end
