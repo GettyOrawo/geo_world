3413a9f8026893ee7dc6e6ea285e598459009b48
Getty Orawo <gettyorawo@gmail.com>
2021-05-25 11:13:11 +0300

    setup Ecto & database configurations

--- lib/geo_world.ex
+++ lib/geo_world.ex
@@ -1,19 +1,17 @@
 defmodule GeoWorld do
   @moduledoc """
-  Documentation for `GeoWorld`.
+  A module for geolocation information
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GeoWorld.hello()
-      :world
-
+  Takes input from CSV file and decodes each line into a map as a record
   """
-  def hello do
-    :world
-  end
 
+  def decode_csv do
+    "data_dump.csv"
+    |> File.stream!
+    |> CSV.decode(separator: ?,, headers: true)
+    |> Enum.take(10) #only for the first 10 records
+    |> Keyword.get_values(:ok)
+  end
 end
