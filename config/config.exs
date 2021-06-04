af07a565a6ed7b282b952d9dc7fdb62639bd4cef
Getty Orawo <gettyorawo@gmail.com>
2021-06-04 12:24:42 +0300

    add csv file

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,10 @@
 import Config
 
-# config :geo_world, GeoWorld.Repo,
-#   database: "geo_world_repo",
-#   username: "postgres",
-#   password: "postgres",
-#   hostname: "localhost"
+config :geo_world, GeoWorld.Repo,
+  database: "geo_world_repo",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost"
 
 config :geo_world,
   ecto_repos: [GeoWorld.Repo]
